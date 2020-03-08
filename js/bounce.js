ca4675e8202c90cd8251d23035aecb218ec124fb
Jonalyn Tenaja <60770488+jonalyntenaja@users.noreply.github.com>
2020-03-08 18:31:54 +0800

    javascript file (not finished) for the website

---
+++ js/bounce.js
@@ -0,0 +1,11 @@
+(function (factory) {
+	if (typeof define === "function" && define.amd) {
+		define(['jquery'], function ($) {
+			return factory($);
+		});
+	} else if (typeof module === "object" && typeof module.exports === "object") {
+		exports = factory(require('jquery'));
+	} else {
+		factory(jQuery);
+	}
+});
